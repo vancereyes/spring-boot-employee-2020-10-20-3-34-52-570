15f4a7bb86843215a3f92f42296fe53a829b6691
REYESVA <vance.albert.reyes@oocl.com>
2020-10-21 16:41:14 +0800

    [employees API][John-Vance]feature = add logic to Employee Service to get all.

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -9,11 +9,15 @@ import java.util.List;
 @Service
 public class EmployeeService {
 
+
+    private final EmployeeRepository repository;
+
     public EmployeeService(EmployeeRepository repository) {
 
+        this.repository = repository;
     }
 
     public List<Employee> getAll() {
-        return null;
+        return repository.findAll();
     }
 }
