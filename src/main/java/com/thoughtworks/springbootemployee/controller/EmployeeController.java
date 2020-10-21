e076322e4af9fe7f908cf18b4e626fcdbafa0424
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-21 15:17:08 +0800

    [employees API][John-Vance]feature - added update().

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -54,5 +55,16 @@ public class EmployeeController {
                 .skip(page * pageSize)
                 .limit(pageSize)
                 .collect(Collectors.toList());
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable int id, @RequestBody Employee updatedEmployee) {
+        employees.stream()
+                .filter(employee -> employee.getId() == id)
+                .findFirst()
+                .ifPresent(employee -> {
+                    employees.remove(employee);
+                    employees.add(updatedEmployee);
+                });
     }
 }
