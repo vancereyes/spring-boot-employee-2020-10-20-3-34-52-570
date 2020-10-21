9b5f14e550d03543d99301538fbcb96ebaf2f54d
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-21 15:09:10 +0800

    [employees API][John-Vance]feature - added paginate().

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,10 +1,18 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -40,5 +48,11 @@ public class EmployeeController {
                 .ifPresent(employee -> employees.remove(employee));
     }
 
-
+    @GetMapping(params = {"page", "pageSize"})
+    public List<Employee> paginate(@RequestParam int page, @RequestParam int pageSize) {
+        return employees.stream()
+                .skip(page * pageSize)
+                .limit(pageSize)
+                .collect(Collectors.toList());
+    }
 }
