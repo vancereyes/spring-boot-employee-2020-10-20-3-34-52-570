f4da88abadabfac7f08d8d5eaaf9d0a266a9fca3
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-21 21:30:06 +0800

    [employees API][John-Vance]fix - add repository annotation to fix error

--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.thoughtworks.springbootemployee.repository;
 
 import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class EmployeeRepository {
     public List<Employee> findAll() {
         return null;
