c66a68693d1c5cdf2a6961fa8ba1948acc41a56e
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-22 09:39:20 +0800

    [Company API][John]feature - add logic to repository

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -3,24 +3,41 @@ package com.thoughtworks.springbootemployee.repository;
 import com.thoughtworks.springbootemployee.model.Company;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class CompanyRepository {
+    private final List<Company> companies = new ArrayList<>();
+
     public List<Company> findAll() {
-        return null;
+        return companies;
     }
 
     public Company save(Company company) {
-        return null;
+        companies.add(company);
+
+        return company;
     }
 
     public Company find(int id) {
-        return null;
+        return companies.stream()
+                .filter(company -> company.getId() == id)
+                .findFirst()
+                .orElse(null);
     }
 
     public Company update(int id, Company updatedCompany) {
-        return null;
+        return companies.stream()
+                .filter(company -> company.getId() == id)
+                .findFirst()
+                .map(company -> {
+                    companies.remove(company);
+                    companies.add(updatedCompany);
+
+                    return updatedCompany;
+                })
+                .orElse(null);
     }
 
     public void delete(Company company) {
