9ed13820606c3ed4bd6d451be65ca445247a139f
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-22 09:39:39 +0800

    [Company API][John]feature - integrate service in controller

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.service.CompanyService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,78 +13,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/companies")
-
 public class CompanyController {
 
-    private final List<Company> companies = new ArrayList<>();
+    private CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
 
     @GetMapping
     public List<Company> getAll() {
-        return companies;
+        return companyService.getAll();
     }
 
     @GetMapping("/{id}")
     public Company get(@PathVariable int id) {
-        return companies.stream()
-                .filter(company -> company.getId() == id)
-                .findFirst()
-                .orElse(null);
+        return companyService.get(id);
     }
 
     @PostMapping
     public Company create(@RequestBody Company company) {
-        companies.add(company);
-
-        return company;
+        return companyService.create(company);
     }
 
     @DeleteMapping("/{id}")
     public Company delete(@PathVariable int id) {
-        return companies.stream()
-                .filter(company -> company.getId() == id)
-                .findFirst()
-                .map(company -> {
-                    company.getEmployees().clear();
-
-                    return company;
-                })
-                .orElse(null);
+        return companyService.clearEmployees(id);
     }
 
     @GetMapping(params = {"page", "pageSize"})
     public List<Company> paginate(@RequestParam int page, @RequestParam int pageSize) {
-        return companies.stream()
-                .skip(pageSize * (page - 1))
-                .limit(pageSize)
-                .collect(Collectors.toList());
+        return companyService.paginate(page, pageSize);
     }
 
     @PutMapping("/{id}")
     public Company update(@PathVariable int id, @RequestBody Company updatedCompany) {
-        return companies.stream()
-                .filter(e -> e.getId() == id)
-                .findFirst()
-                .map(e -> {
-                    companies.remove(e);
-                    companies.add(updatedCompany);
-
-                    return updatedCompany;
-                })
-                .orElse(null);
+        return companyService.update(id, updatedCompany);
     }
 
     @GetMapping("/{id}/employees")
     public List<Employee> getCompanyEmployees(@PathVariable int id) {
-        return companies.stream()
-                .filter(company -> company.getId() == id)
-                .findFirst()
-                .map(Company::getEmployees)
-                .orElse(null);
+        return companyService.getEmployees(id);
     }
 }
